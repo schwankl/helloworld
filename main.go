2482bb6e3dd89c4e1fc07940be7ddd7e3e404e90
vitaliyzinchenko <captzika@gmail.com>
2018-10-26 11:43:51 -0400

    special handling for Kire

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path[1:] != "navarkos" {
 		fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	} else if r.URL.Path[1:] == "Kire" {
+		fmt.Fprintf(w, "Hi there, Kire what you had for breakfast!")
 	} else {
 		fmt.Fprintf(w, "Hi there, Navarkos is the best thing that ever happened to kubernetes community!")
 	}
